a5a4c4d0d45defcd213c37c2d44fabfcdbe76d4d
Sean Daniels <daniels2@mskcc.org>
2020-09-25 16:34:57 -0400

    Add labels for anchor links

--- public/aria-labels.js
+++ public/aria-labels.js
@@ -1,31 +1,41 @@
-const buttonLinks = Array.prototype.slice.call(document.getElementsByTagName("button"), 0);
-const anchorLinks = Array.prototype.slice.call(document.getElementsByTagName("a"), 0);
-const buttons = buttonLinks.concat(anchorLinks)
+const buttonLinks = Array.prototype.slice.call(
+  document.getElementsByTagName("button"),
+  0
+);
+const anchorLinks = Array.prototype.slice.call(
+  document.getElementsByTagName("a"),
+  0
+);
+const buttons = buttonLinks.concat(anchorLinks);
 
 function addAriaLabels() {
   for (let i = 0; i < buttons.length; i++) {
     if (
-      !buttons[i].getAttribute('aria-label') &&
-      buttons[i].textContent.toLowerCase() === "read more" ||
+      (!buttons[i].getAttribute("aria-label") &&
+        buttons[i].textContent.toLowerCase() === "read more") ||
       buttons[i].textContent.toLowerCase() === "learn more"
     ) {
-      const buttonLink = buttons[i].getAttribute("href");
-      let buttonAriaLabel;
-      fetch(buttonLink)
-        .then((response) => response.text())
-        .then((text) => {
-          var parsedResponse = new window.DOMParser().parseFromString(
-            text,
-            "text/html"
-          );
-          buttonAriaLabel = parsedResponse.title;
-          buttons[i].setAttribute(
-            "aria-label",
-            `Link to ${buttonAriaLabel} page`
-          );
-        });
+      if (buttons[i].getAttribute("href")[0] == "#") {
+        buttons[i].setAttribute("aria-label", "Anchor link within page");
+      } else {
+        const buttonLink = buttons[i].getAttribute("href");
+        let buttonAriaLabel;
+        fetch(buttonLink)
+          .then((response) => response.text())
+          .then((text) => {
+            var parsedResponse = new window.DOMParser().parseFromString(
+              text,
+              "text/html"
+            );
+            buttonAriaLabel = parsedResponse.title;
+            buttons[i].setAttribute(
+              "aria-label",
+              `Link to ${buttonAriaLabel} page`
+            );
+          });
+      }
     }
   }
 }
 
-addAriaLabels()
+addAriaLabels();
